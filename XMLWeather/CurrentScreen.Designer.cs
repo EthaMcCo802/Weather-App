db092b5b9f769a71f9a1bffd317f3847197e4087
Ethan McComb <ethamcco802@ed.amdsb.ca>
2021-11-05 17:08:01 -0400

    None noticable

--- XMLWeather/CurrentScreen.Designer.cs
+++ XMLWeather/CurrentScreen.Designer.cs
@@ -154,7 +154,7 @@
             this.label1.BackColor = System.Drawing.Color.DarkGray;
             this.label1.Location = new System.Drawing.Point(283, 226);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(289, 180);
+            this.label1.Size = new System.Drawing.Size(302, 180);
             this.label1.TabIndex = 44;
             this.label1.UseWaitCursor = true;
             // 
@@ -259,9 +259,9 @@
             // label3
             // 
             this.label3.BackColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(3, 226);
+            this.label3.Location = new System.Drawing.Point(-5, 226);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(282, 180);
+            this.label3.Size = new System.Drawing.Size(290, 180);
             this.label3.TabIndex = 76;
             // 
             // label4
